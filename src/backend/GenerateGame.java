eb4aac00253a6ffce9372ae1a054208bb9ec225a
David Mansour <davidyassa@hotmail.com>
2025-12-22 02:08:40 +0200

    corrected RandomPairs class from V2

--- src/backend/GenerateGame.java
+++ src/backend/GenerateGame.java
@@ -36,24 +36,8 @@ public class GenerateGame {
 
     public int[][] generate() {
         int[][] generated = csvManager.clone(board);
-        Set<Integer> removed = new HashSet<>();
-
-        int target = pairs.size();
-        // had to use this loop to avoid collisions
-        while (removed.size() < target) {
-            List<int[]> batch = rnd.generateDistinctPairs(target);
-
-            for (int[] pair : batch) {
-                int cell = pair[0]; // 0..80
-                if (removed.add(cell)) {
-                    int r = cell / 9;
-                    int c = cell % 9;
-                    generated[r][c] = 0;
-                    if (removed.size() == target) {
-                        break;
-                    }
-                }
-            }
+        for (int[] pair : pairs) {
+            generated[pair[0]][pair[1]] = 0;
         }
 
         return generated;
